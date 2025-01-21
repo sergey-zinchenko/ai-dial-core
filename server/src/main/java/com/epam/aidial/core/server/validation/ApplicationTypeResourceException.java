69116f09baf01f4617005fdede108d0684daeb53
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2025-01-21 13:28:51 +0100

    doc

--- server/src/main/java/com/epam/aidial/core/server/validation/ApplicationTypeResourceException.java
+++ server/src/main/java/com/epam/aidial/core/server/validation/ApplicationTypeResourceException.java
@@ -2,6 +2,12 @@ package com.epam.aidial.core.server.validation;
 
 import lombok.Getter;
 
+
+/**
+ * Exception thrown when there is an issue with a resource associated with an application type.
+ * This exception is typically used when a resource listed as dependent to the application
+ * is not found, inaccessible, or there is a failure in obtaining the resource descriptor.
+ */
 @Getter
 public class ApplicationTypeResourceException extends RuntimeException {
 
